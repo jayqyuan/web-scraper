014818f7f6e4b24b95b44248119eac250deee373
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-12-21 00:11:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+#! python3
 from bs4 import BeautifulSoup
 
+with open('home.html', 'r') as html_file:
+    content = html_file.read()
+    
+    soup = BeautifulSoup(content, 'lxml')
+    courses_cards = soup.findAll('div', class_ ='cards' )
+    
+    for card in courses_cards:
+        print(card.h5)
