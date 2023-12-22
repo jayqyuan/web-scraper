a6d9820c66b4721f34bdc4bc8dfc3d82a9152717
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-12-22 10:47:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #! python3
 from bs4 import BeautifulSoup
+import requests
 
-with open('home.html', 'r') as html_file:
-    content = html_file.read()
-    
-    soup = BeautifulSoup(content, 'lxml')
-    courses_cards = soup.find_all('div', class_ ='card' )
-    
-    for card in courses_cards:
-        course_name = card.h5.text
-        course_price = card.a.text.split()[-1]
+html_text = requests.get(
+    'https://www.indeed.com/jobs?q=full+stack+developer&l=New+York%2C+NY&from=searchOnHP&vjk=ea2a34b0cf2515cc')
 
-        print(f'{course_name} costs {course_price}')
+# with open('home.html', 'r') as html_file:
+#     content = html_file.read()
+    
+#     soup = BeautifulSoup(content, 'lxml')
+#     courses_cards = soup.find_all('div', class_ ='card' )
+    
+#     for card in courses_cards:
+#         course_name = card.h5.text
+#         course_price = card.a.text.split()[-1]
+
+#         print(f'{course_name} costs {course_price}')
